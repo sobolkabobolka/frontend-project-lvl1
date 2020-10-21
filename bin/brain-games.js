8903c28348626da62865a5034d5d9bba67efd9e8
Anastasiia Sobol <sobolek7@yandex.ru>
2020-10-21 16:42:34 +0300

    Github action linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import cli from '../src/cli.mjs';
+import cli from '../src/cli';
 
 console.log('Welcome to the Brain Games!');
 
--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import promptly from 'promptly';
 
 export default async () => {
   const name = await promptly.prompt('May I have your name? ');
-
-  console.log('Hello, ' + name + '!');
+  let a;
+  console.log(`Hello, ${name}!`);
 };
